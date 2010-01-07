02eec1ab3e15370246a2d787eb61279a92b1bb06
Anthony Kolber <ae@aestheticallyloyal.com>
2010-01-07 18:07:30 +1100

    Allow hash tags through the navigation selector js

--- public/docs/js/navigation.js
+++ public/docs/js/navigation.js
@@ -1,5 +1,5 @@
 var nav_items = document.getElementById('inner-navigation').getElementsByTagName('a');
-var stripped_url = document.location.href.match(/([^\/]+)\/$/)[1];
+var stripped_url = document.location.href.match(/([^\/]+)\/(#.+)?$/)[1];
 for(var i = 0; i < nav_items.length; i++) {
   var slug = nav_items[i].href.match(/([^\/]+)\/$/);
 	var stripped_href = (slug) ? slug[1] : '';
